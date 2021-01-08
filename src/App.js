abb19fcd6aef0293fa2617a0a3fea11389a3dada
Abou Bkar Siddiq <aboubkar786@gmail.com>
2021-01-08 23:40:17 +0500

    UI Completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Grid from './components/grid'
+import Footer from './components/footer'
+import Achievements from './components/achievements';
+import Headline from './components/headline';
+import Worldoffashion from './components/worldoffashion';
+import Getstarted from './components/getstarted';
+import Logos from './components/logos';
+import Stories from './components/stories';
+import FooterMini from './components/footermini';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Getstarted />
+      <Logos />
+      <Grid />
+      <Stories />
+      <Achievements />
+      <Headline />
+      <Worldoffashion />
+      <Footer />
+      <FooterMini />
     </div>
   );
 }
-
 export default App;
